7bccecf1a9eaa8f18702e198a700e1163625155b
ahmed000372 <ahmed.abualgasim778899@gmail.com>
2025-05-16 20:35:45 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['218942367390'] //ur owner number
+global.ownername = "𝐆𝐎𝐇𝐀𝐍" //ur owner name
+global.ytname = "𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 " //ur yt chanel name
+global.socialm = "GitHub: 𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 " //ur github or insta name
+global.location = "Africa, libya, misratta" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+glo" //ur number
+global.premium = ['218930171336'] //ur premium number
+global.botname = '❤️𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 ❤️' //ur bot name
+global.linkz = "𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 " //your theme url which will be displayed on whatsapp
+global.websitex = "𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 " //ur website to be displayed
+global.botscript = '𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 ' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 ⍣" //ur sticker watermark packname
+global.author = "⍣218942367390⍣" //ur sticker watermark author
+global.wm = "𝐆𝐎𝐇𝐀𝐍 𝐁𝐎𝐓 ." //ur watermark
 
 // Other
 global.sessionName = 'session'
